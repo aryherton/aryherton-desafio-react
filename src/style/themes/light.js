97912687491d9d27c4ecc5421c7ade08584815a2
Ary Barbosa <aryhertonbarbosa@gmail.com>
2022-08-07 18:09:31 -0300

    feat: created file "light.js" in folder "./src/style/themes"

---
+++ src/style/themes/light.js
@@ -0,0 +1,12 @@
+export default {
+    colors: {
+        pageSearchBG: '#FAFAFA',
+        pageHomeBG: '#FFFFFF',
+        buttonSearchBG: '#46535D',
+        buttonFollowBG: '#FAFBFC',
+        fontNameRepos: '#0366D6',
+        marksStacks: '#4F5D95',
+        marksStackHTML: '#E34C26',
+        placeholderInput: '#98a7b7',
+    }
+};
