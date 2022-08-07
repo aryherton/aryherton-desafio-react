ca93098177318e2ccdd2e3603c050bc4550b1874
Ary Barbosa <aryhertonbarbosa@gmail.com>
2022-08-07 18:04:11 -0300

    feat: inserted "GlobalStyle"

--- src/App.js
+++ src/App.js
@@ -5,16 +5,22 @@ import DataProvider from './context/DataProvider';
 import Home from './pages/Home';
 import NotFound from './pages/NotFound';
 import Search from './pages/Search';
+import { ThemeProvider } from 'styled-components';
+import light from './style/themes/light';
+import GlobalStyle from './style/global/globalStyle';
 
 function App() {
   return (
-    <DataProvider>
-      <Routes>
-        <Route path="/home" element={<Home />} />
-        <Route exact path="/" element={<Search />} />
-        <Route path="*" element={<NotFound />} />
-      </Routes>
-    </DataProvider>
+    <ThemeProvider theme={light}>
+      <GlobalStyle />
+      <DataProvider>
+        <Routes>
+          <Route path="/home" element={<Home />} />
+          <Route exact path="/" element={<Search />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </DataProvider>
+    </ThemeProvider>
   );
 }
 
