72cd69581e6b1120de2a7c22b7d0b926f12580ad
Ary Barbosa <aryhertonbarbosa@gmail.com>
2022-08-07 18:28:36 -0300

    feat: inserted "useEffect" in file "./src/pages/Home.jsx"

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,16 @@
-import React from 'react';
+import React, { useEffect, useContext } from 'react';
+import { useNavigate } from 'react-router-dom';
+
+import DataContext from '../context/DataContext';
 
 function Home() {
+  const navigate = useNavigate();
+  const { dataUser } = useContext(DataContext);
+  useEffect(() => {
+    if (!dataUser) {
+      navigate('/');
+    }
+  }, []);
   return (
     <div>
       <h1>Home</h1>
