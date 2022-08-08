0738cd6bbdd23474afe17bee713f7b5143b69c7b
Ary Barbosa <aryhertonbarbosa@gmail.com>
2022-08-07 22:48:08 -0300

    feat: created file "styledPageHome.js" in folder "./src/style/pagesStyle"

---
+++ src/style/pagesStyle/styledPageHome.js
@@ -0,0 +1,94 @@
+import styled from 'styled-components';
+
+const StyledHomeWrapper = styled.div `
+  width: 100%;
+  height: 100vh;
+  main {
+    display: flex;
+    width: 100%;
+    height: 100%;
+    section {
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      width: 43%;
+      height: 100%;
+      #divBackImg {
+        width: 360px;
+        height: 330px;
+      }
+      #imgUser {
+        position: absolute;
+        margin-top: -40px;
+        border-radius: 50%;
+        width: 360px;
+        height: 360px;
+      }
+      #nameUser {
+        font-family: 'Noto Sans', sans-serif;
+        letter-spacing: 1px;
+        width: 360px;
+        height: 70px;
+        align-self: center;
+      }
+      button {
+        width: 360px;
+        height: 40px;
+        background-color: ${props => props.theme.colors.buttonFollowBG};
+        border: 1px solid #c4c9ce;
+        border-radius: 5px;
+      }
+      p {
+        margin-top: 20px;
+        font-family: 'Noto Sans', sans-serif;
+        width: 360px;
+        font-weight: bolder;
+      }
+      #followers {
+        display: flex;
+        align-items: center;
+        width: 360px;
+        font-size: 14px;
+        height: 40px;
+        font-family: 'Open Sans', sans-serif;
+        margin-left: 5px;
+        font-family: 'Noto Sans', sans-serif;
+        img {
+          width: 20px;
+          height: 20px;
+        }
+        #icon_people {
+          margin-right: 5px;
+        }
+        span {
+          font-weight: bolder;
+        }
+        .txtFollowers {
+          font-weight: normal;
+          margin-left: 5px;
+          margin-right: 5px;
+        }
+      }
+      #divImgEmail {
+        border-bottom: 1px solid #c4c9ce;
+        width: 360px;
+        height: 45px;
+        display: flex;
+        align-items: center;
+        font-family: 'Noto Sans', sans-serif;
+        #imgEmail {
+          width: 20px;
+        }
+      }
+    }
+    aside {
+      width: 57%;
+      height: 100%;
+      background-color: yellow;
+    }
+  }
+`;
+
+
+
+export default StyledHomeWrapper;
