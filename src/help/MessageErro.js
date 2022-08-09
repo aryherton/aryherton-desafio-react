55256164db39dc3b5a375e2ed628a62b317613da
Ary Barbosa <aryhertonbarbosa@gmail.com>
2022-08-08 23:22:07 -0300

    feat: created file "MessageErro.js" in folder "./src/help"

---
+++ src/help/MessageErro.js
@@ -0,0 +1,6 @@
+const ERROR_MESSAGE = {
+  not_found: 'Usuário não encontrado no github.Verifique se você digitou o nome corretamente.',
+  input_empty: 'Informe um nome de usuário válido do github.'
+}
+
+export default ERROR_MESSAGE;
